1d976244f0293f3998d422f24ca7fc29ba06a9ab
Taiane Rodrigues <Taiane-s.r@hotmail.com>
2021-09-06 21:10:59 -0300

    atualizações

--- calculadora/src/main/java/br/com/undefined/calculadora/controller/LaboratorioController.java
+++ calculadora/src/main/java/br/com/undefined/calculadora/controller/LaboratorioController.java
@@ -2,9 +2,16 @@ package br.com.undefined.calculadora.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.undefined.calculadora.controller.form.LaboratorioForm;
+import br.com.undefined.calculadora.dto.LaboratorioDto;
 import br.com.undefined.calculadora.model.Laboratorio;
 import br.com.undefined.calculadora.service.LaboratorioService;
 
@@ -14,9 +21,21 @@ public class LaboratorioController {
 
 	private LaboratorioService laboratorioService;
 	
-	@GetMapping
+	@GetMapping("/listar")
 	public List<Laboratorio> listarMedicamentos(){
 		return laboratorioService.listar();
+		
+	}
+	
+	
+	@PostMapping("/criar")
+	public ResponseEntity<LaboratorioDto> criar(@RequestBody LaboratorioForm laboratorioForm ) {
+		
+		
+		
+		return null;
+		
+		
 		
 	}
 	
--- calculadora/src/main/java/br/com/undefined/calculadora/service/LaboratorioService.java
+++ calculadora/src/main/java/br/com/undefined/calculadora/service/LaboratorioService.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.undefined.calculadora.dto.LaboratorioDto;
 import br.com.undefined.calculadora.model.Laboratorio;
+import br.com.undefined.calculadora.modelMapper.ModelMapperConfig;
 import br.com.undefined.calculadora.repository.LaboratorioRepository;
 
 @Service
@@ -14,12 +16,16 @@ public class LaboratorioService {
 	@Autowired
 	private LaboratorioRepository laboratorioRepository;
 	
+	private ModelMapperConfig modelMapper;
+	
 	public List<Laboratorio> listar(){
 		List<Laboratorio> lst= laboratorioRepository.findAll();
 		return lst;
 	}
 	
 	
-	
+	  private LaboratorioDto map(Laboratorio laboratorio) {
+	        return this.modelMapper.modelMapper().map(laboratorio, LaboratorioDto.class);
+	    }
 
 }
