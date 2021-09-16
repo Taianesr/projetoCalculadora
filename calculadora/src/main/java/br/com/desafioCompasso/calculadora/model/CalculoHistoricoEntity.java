2c101ff4622d68c515cc90eff652664a4a2abae6
Taiane Rodrigues <Taiane-s.r@hotmail.com>
2021-09-16 17:41:30 -0300

    Alteração classes entity.

--- calculadora/src/main/java/br/com/desafioCompasso/calculadora/model/CalculoHistoricoEntity.java
+++ calculadora/src/main/java/br/com/desafioCompasso/calculadora/model/CalculoHistoricoEntity.java
@@ -1,13 +1,110 @@
 package br.com.desafioCompasso.calculadora.model;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="calculo_historico")
 public class CalculoHistoricoEntity {
 	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	private datetime dataCalculo;
+	//ou date time?
+	private Date dataCalculo;
 	private String nomeUsuario;
 	private Long idMedicamento;
 	private ViaAdministracaoEntity viaAdmEntity;
-	private float quantidadePrescrita;
+	private BigDecimal quantidadePrescrita;
+	private String resultadosJson;
+	// falta parametro ? private String viaAdministracao; 
+	
+	public CalculoHistoricoEntity() {
+		super();
+	}
+
+
+	public CalculoHistoricoEntity(Long id, Date dataCalculo, String nomeUsuario, Long idMedicamento,
+			ViaAdministracaoEntity viaAdmEntity, BigDecimal quantidadePrescrita, String resultadosJson) {
+		super();
+		this.id = id;
+		this.dataCalculo = dataCalculo;
+		this.nomeUsuario = nomeUsuario;
+		this.idMedicamento = idMedicamento;
+		this.viaAdmEntity = viaAdmEntity;
+		this.quantidadePrescrita = quantidadePrescrita;
+		this.resultadosJson = resultadosJson;
+	}
+
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public Date getDataCalculo() {
+		return dataCalculo;
+	}
+
+
+	public void setDataCalculo(Date dataCalculo) {
+		this.dataCalculo = dataCalculo;
+	}
+
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+
+	public Long getIdMedicamento() {
+		return idMedicamento;
+	}
+
+
+	public void setIdMedicamento(Long idMedicamento) {
+		this.idMedicamento = idMedicamento;
+	}
+
+
+	public ViaAdministracaoEntity getViaAdmEntity() {
+		return viaAdmEntity;
+	}
+
+
+	public void setViaAdmEntity(ViaAdministracaoEntity viaAdmEntity) {
+		this.viaAdmEntity = viaAdmEntity;
+	}
+
+	public String getResultadosJson() {
+		return resultadosJson;
+	}
+
+
+	public void setResultadosJson(String resultadosJson) {
+		this.resultadosJson = resultadosJson;
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 
--- calculadora/src/main/java/br/com/desafioCompasso/calculadora/model/DiluicaoConfiguracaoEntity.java
+++ calculadora/src/main/java/br/com/desafioCompasso/calculadora/model/DiluicaoConfiguracaoEntity.java
@@ -3,20 +3,20 @@ package br.com.desafioCompasso.calculadora.model;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name ="diluicao_configuracao")
 public class DiluicaoConfiguracaoEntity {
-    
-
-	//ou guardar o objeto medicamento
-	private Long medicamentoId;
-	//guardar objeto via administracao
-	private Long viaAdministracaoId;
+   
+	@ManyToOne
+	@JoinColumn(name = "medicamento_id", referencedColumnName = "id" )
+	private MedicamentoEntity medicamento;
+	@ManyToOne
+	@JoinColumn(name = "via_administracao_id", referencedColumnName = "id" )
+	private ViaAdministracaoEntity viaAdministracao;
 	private int sequencia;
 	private BigDecimal quantidadeAspirada;
 	private BigDecimal quantidadeAdicionada;
@@ -24,91 +24,8 @@ public class DiluicaoConfiguracaoEntity {
 	private String diluente;
 	private String modoPreparo;
 
-	public DiluicaoConfiguracaoEntity() {
-		super();
-	}
 
-
-	public DiluicaoConfiguracaoEntity( Long medicamentoId, Long viaAdministracaoId, int sequencia,
-			BigDecimal quantidadeAspirada, BigDecimal quantidadeAdicionada, BigDecimal concentracao, String diluente,
-			String modoPreparo) {
-		super();
-		this.medicamentoId = medicamentoId;
-		this.viaAdministracaoId = viaAdministracaoId;
-		this.sequencia = sequencia;
-		this.quantidadeAspirada = quantidadeAspirada;
-		this.quantidadeAdicionada = quantidadeAdicionada;
-		this.concentracao = concentracao;
-		this.diluente = diluente;
-		this.modoPreparo = modoPreparo;
-	}
-
-
-
-	public Long getMedicamentoId() {
-		return medicamentoId;
-	}
-
-	public void setMedicamentoId(Long medicamentoId) {
-		this.medicamentoId = medicamentoId;
-	}
-
-	public Long getViaAdministracaoId() {
-		return viaAdministracaoId;
-	}
-
-	public void setViaAdministracaoId(Long viaAdministracaoId) {
-		this.viaAdministracaoId = viaAdministracaoId;
-	}
-
-	public int getSequencia() {
-		return sequencia;
-	}
-
-	public void setSequencia(int sequencia) {
-		this.sequencia = sequencia;
-	}
-
-	public BigDecimal getQuantidadeAspirada() {
-		return quantidadeAspirada;
-	}
-
-	public void setQuantidadeAspirada(BigDecimal quantidadeAspirada) {
-		this.quantidadeAspirada = quantidadeAspirada;
-	}
-
-	public BigDecimal getQuantidadeAdicionada() {
-		return quantidadeAdicionada;
-	}
-
-	public void setQuantidadeAdicionada(BigDecimal quantidadeAdicionada) {
-		this.quantidadeAdicionada = quantidadeAdicionada;
-	}
-
-	public BigDecimal getConcentracao() {
-		return concentracao;
-	}
-
-	public void setConcentracao(BigDecimal concentracao) {
-		this.concentracao = concentracao;
-	}
-
-	public String getDiluente() {
-		return diluente;
-	}
-
-	public void setDiluente(String diluente) {
-		this.diluente = diluente;
-	}
-
-	public String getModoPreparo() {
-		return modoPreparo;
-	}
-
-	public void setModoPreparo(String modoPreparo) {
-		this.modoPreparo = modoPreparo;
-	}
-
+    
 
 
 
